229cee295f0c2a91d8937eaa9828dd43e7e3e517
sfan5 <sfan5@live.de>
2017-08-03 22:55:20 +0200

    Add support for youtube-dl

--- app/src/main/java/is/xyz/mpv/MPVActivity.kt
+++ app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -220,8 +220,12 @@ class MPVActivity : Activity(), MPVLib.EventObserver, TouchGesturesObserver {
 
     private fun copyAssets() {
         val assetManager = applicationContext.assets
-        val files = arrayOf("subfont.ttf", "cacert.pem")
+        val files = arrayOf(
+            "subfont.ttf", "cacert.pem", "youtube-dl",
+            "ytdl/python3", "ytdl/python38.zip", "ytdl/youtube-dl"
+        )
         val configDir = applicationContext.filesDir.path
+        File("$configDir/ytdl").mkdir()
         for (filename in files) {
             var ins: InputStream? = null
             var out: OutputStream? = null
@@ -243,6 +247,13 @@ class MPVActivity : Activity(), MPVLib.EventObserver, TouchGesturesObserver {
                 ins?.close()
                 out?.close()
             }
+        }
+
+        val execFiles = arrayOf("youtube-dl", "ytdl/python3")
+        for (filename in execFiles) {
+            try {
+                File("$configDir/$filename").setExecutable(true)
+            } catch (e: IOException) {}
         }
     }
 
